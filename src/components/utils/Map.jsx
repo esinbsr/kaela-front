280de4206267182f5ce6dc8888b26a706cc27d3c
Esin <hokablese@gmail.com>
2024-08-22 18:48:35 +0200

    current contact page style

---
+++ src/components/utils/Map.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = ({ address }) => {
+  const mapRef = useRef(null); // Utilisation d'un ref pour stocker l'instance de la carte
+  const coordinatesRef = useRef([51.505, -0.09]); // Coordonnées par défaut
+
+  useEffect(() => {
+    if (address) {
+      const fetchCoordinates = async () => {
+        const response = await fetch(
+          `https://nominatim.openstreetmap.org/search?format=json&q=${address}`
+        );
+        const data = await response.json();
+        if (data.length > 0) {
+          coordinatesRef.current = [data[0].lat, data[0].lon];
+          if (mapRef.current) {
+            mapRef.current.setView(coordinatesRef.current, 13);
+            L.marker(coordinatesRef.current).addTo(mapRef.current);
+          }
+        }
+      };
+      fetchCoordinates();
+    }
+  }, [address]);
+
+  useEffect(() => {
+    if (!mapRef.current) {
+      mapRef.current = L.map('map').setView(coordinatesRef.current, 13);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors',
+      }).addTo(mapRef.current);
+    }
+  }, []);
+
+  return <div id="map" style={{ height: '400px', width: '100%' }}></div>;
+};
+
+export default Map;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import SocialNetworkIcon from "../components/utils/SocialNetworkIcon";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faLocationDot } from "@fortawesome/free-solid-svg-icons"; 
+import { useDispatch, useSelector } from "react-redux";
+import { getInformation } from "../actions/informationAction";
+import { isEmpty } from "../components/utils/isEmpty";
+import Map from "../components/utils/Map";
 
 const Contact = () => {
+  const dispatch = useDispatch();
+  const information = useSelector((state) => state.information.information);
+
+  useEffect(() => {
+    dispatch(getInformation());
+  }, [dispatch]);
+
+  const info = !isEmpty(information) ? information[0] : null;
+
   const [formData, setFormData] = useState({
     email: "",
     object: "",
@@ -24,7 +39,6 @@ const Contact = () => {
     try {
       const response = await axios.post(
         "http://localhost:8888/travail-perso/kaela-couture/contact",
-        
         formData,
         {
           headers: {
@@ -45,39 +59,49 @@ const Contact = () => {
   };
 
   return (
-    <div>
-         <SocialNetworkIcon/>
-    <h1>Contact me</h1>
-    <form onSubmit={handleSubmit} className="user-form">
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="text"
-        name="email"
-        value={email}
-        onChange={handleChange}
-      />
-      <label htmlFor="object">Object</label>
-      <input
-        id="object"
-        type="text"
-        name="object"
-        value={object}
-        onChange={handleChange}
-      />
-      <label htmlFor="message">Message</label>
-      <textarea
-        id="message"
-        name="message"
-        value={message}
-        onChange={handleChange}
-      ></textarea>
-      <button type="submit">Send</button>
-    </form>
-    
-    {/* {error && <Message message={error} type="error" />} */}
-  </div>
-  
+    <div className="contact">
+      <SocialNetworkIcon />
+      <form onSubmit={handleSubmit} className="contact__form">
+        <h1>Contact me</h1>
+
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
+        <label htmlFor="object">Object</label>
+        <input
+          id="object"
+          type="text"
+          name="object"
+          value={object}
+          onChange={handleChange}
+        />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
+      <div className="contact__data">
+        <div className="contact__mobile">
+          <FontAwesomeIcon icon={faPhone} /> 
+          {info && <p>+{info.mobile}</p>}
+        </div>
+
+        <div className="contact__localisation">
+          <FontAwesomeIcon icon={faLocationDot} /> 
+          {info && <p>{info.address}</p>}
+        </div>
+      {info && <Map address={info.address} />}
+      </div>
+    </div>
   );
 };
 
